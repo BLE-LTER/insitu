e603ffa99d815948213047c8f3ce7160658888b9
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-10-25 13:07:37 -0500

    refactor cont: I forgot R is vectorized so removing loop from condtosal

--- R/CondtoSal_miliS.R
+++ R/CondtoSal_miliS.R
@@ -11,23 +11,21 @@ Cond.to.Sal <- function(cond, temp, ref_cond = 42.9){
 
   if (length(cond) != length(temp)) error("Please supply vectors of same lengths for cond and sal.")
   if (length(ref_cond) > 1) error("Please supply only one reference conductivity.")
+  if (!is.numeric(cond) | !is.numeric(temp) | !is.numeric(ref_cond)) error("Please supply numeric values.")
 
-  Sal <- c()
-  for (i in 1:length(cond)) {
   # this is the conductivity ratio. 42.9 is the reference conductivity in mS/cm. If you measured conductivity in different units, you will need to change the reference conductivity to match.
-  var1 <- cond[i]/ref_cond
+  var1 <- cond/ref_cond
 
   # rt
-  var2 <- 0.6766097 + (0.0200564 * temp[i]) + (0.0001104259 * temp[i]^2) + ((-6.9698 * 10^-7) * temp[i]^3) + ((1.0031 * 10^-9) * temp[i]^4)
+  var2 <- 0.6766097 + (0.0200564 * temp) + (0.0001104259 * temp^2) + ((-6.9698 * 10^-7) * temp^3) + ((1.0031 * 10^-9) * temp^4)
 
   # Rt
   var3 <- var1/var2
 
   # dS
-  var4 <- ((temp[i] - 15) / (1 + (0.0162 * (temp[i] - 15)))) * (0.0005 + ((-0.0056) * var3^0.5) + ((-0.0066) * var3) + ((-0.0375) * var3^1.5) + ((0.0636) * var3^2) + ((-0.0144) * var3^2.5))
+  var4 <- ((temp - 15) / (1 + (0.0162 * (temp - 15)))) * (0.0005 + ((-0.0056) * var3^0.5) + ((-0.0066) * var3) + ((-0.0375) * var3^1.5) + ((0.0636) * var3^2) + ((-0.0144) * var3^2.5))
 
-  Sal[i] <- 0.008 + ((-0.1692) * var3^0.5) + (25.3851 * var3) + (14.0941 * var3^1.5) + ((-7.0261) * var3^2) + (2.7081 * var3^2.5) + var4
-  }
+  Sal <- 0.008 + ((-0.1692) * var3^0.5) + (25.3851 * var3) + (14.0941 * var3^1.5) + ((-7.0261) * var3^2) + (2.7081 * var3^2.5) + var4
 
   return(Sal)
 }
