271dbfa041f6e79b3d583c23126d2a88b3799cbb
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-11-14 12:12:18 -0600

    rename TS.adj to adjust_linear

--- R/TS.adj.R
+++ R/TS.adj.R
@@ -1,15 +1,13 @@
-#' Calibrate data
-#' Based on initial calibration measurment and end calibration measurement. Adjustment is linear.
-#' @param raw (numeric) Numeric vector of raw data.
-#' @param calib (numeric) df column that is blank except for the calibration values
-#' @param rowfirst (numeric) the rownumber of the first calibration value
-#' @param rowlast (numeric) the rownumber of the second (and last) calibration value
-#' @param rownums a column of row numbers.
+#' Linearly adjust data based on two data points of know quality.
+#' @param raw (numeric) Numeric vector of raw data, sorted by date time ascending.
+#' @param calib (numeric) Numeric vector that is blank except for the calibration values.
+#' @param rowfirst (numeric) Rownumber of the first calibration value.
+#' @param rowlast (numeric) Rownumber of the second (and last) calibration value.
+#' @param rownums (numeric) Vector of row numbers. Defaults to 1:length(raw)
 #' @return A vector with caliberated values. Adjustment is linear.
 #' @export
 
-TS.adj <- function (raw, calib, rowfirst, rowlast, rownums) {
-
+adjust_linear <- function(raw, calib, rowfirst, rowlast, rownums = 1:length(raw)) {
   x1 <- calib[rowfirst] - raw[rowfirst]
   x2 <- calib[rowlast] - raw[rowlast]
   xslope <- (x2 - x1) / (rowlast - rowfirst)
