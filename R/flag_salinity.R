3f5695b6fee8e034f7b4e6a2e3ef40d0e7c54510
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-11-15 17:22:05 -0600

    fix bug in flag_salinity

--- R/flag_salinity.R
+++ R/flag_salinity.R
@@ -7,26 +7,36 @@
 #' @param condcol (character) Name of column for conductivity. Defaults to "conductivity".
 #' @param Terror (numeric) Precision for temperature.
 #' @param Cerror (numeric) Precision for conductivity.
-#' @param flag_scheme (character) Scheme for flagging anomalous salinity values. Defaults to NULL and outputs a logical TRUE/FALSE column. Alternatively supply a vector of two character flags to denote good/bad data respectively, e.g. c("valid", "invalid").
-#' @return (data.frame) The input dataframe with new columns added: "anomalous" (logical) indicating anomalous values, "salinity" (named according to input argument salcol) for calculated salinity.
+#' @param flag_scheme (character) Scheme for flagging anomalous salinity values. Supply a vector of character flags to denote good/bad data respectively, e.g. c("valid", "invalid"). The first two will be used. IF NULL function will output a logical TRUE/FALSE column.
+#' @return (data.frame) The input dataframe with a new column added: "anomalous" (character or logical) indicating anomalous values.
 #' @export
 
-flag_salinity <- function(data, tempcol = "temperature", condcol = "conductivity", Terror, Cerror, flag_scheme = NULL) {
+flag_salinity <-
+  function(data,
+           tempcol = "temperature",
+           condcol = "conductivity",
+           Terror,
+           Cerror,
+           flag_scheme = c("valid", "invalid")) {
+    if (!tempcol %in% names(data)) stop(paste(tempcol, "not a column in data"))
+    if (!condcol %in% names(data)) stop(paste(condcol, "not a column in data"))
+    stopifnot(is.numeric(Terror), is.numeric(Cerror), is.null(flag_scheme) | length(flag_scheme) >= 2)
 
     posTerror <- data[[tempcol]] + Terror
-  posCerror <- data[[condcol]] + Cerror
-  pCpTSalerror <- calculate_salinity(posCerror, posTerror)
+    posCerror <- data[[condcol]] + Cerror
+    pCpTSalerror <- calculate_salinity(posCerror, posTerror)
 
-  # data is "anomalous" if data +C+T error is below the freezing line
-  data[["anomalous"]] <-
-    posTerror < (-0.0575 * pCpTSalerror) + (pCpTSalerror^1.5 * 1.710523E-3) - (2.154996E-4 * pCpTSalerror ^ 2) - 7.53E-4
+    # data is "anomalous" if data +C+T error is below the freezing line
+    data[["anomalous"]] <-
+      posTerror < (-0.0575 * pCpTSalerror) + (pCpTSalerror ^ 1.5 * 1.710523E-3) - (2.154996E-4 * pCpTSalerror ^ 2) - 7.53E-4
 
-  if (!is.null(flag_scheme)) {
-    if (is.character(flag_scheme) && length(flag_scheme) == 2){
-      data[data[["anomalous"]], "anomalous"] <- as.character(flag_scheme[1])
-      data[!data[["anomalous"]], "anomalous"] <- as.character(flag_scheme[2])
+    if (!is.null(flag_scheme)) {
+      if (is.character(flag_scheme) && length(flag_scheme) == 2) {
+        data[data[["anomalous"]], "anomalous"] <-
+          as.character(flag_scheme[1])
+        data[data[["anomalous"]] == FALSE, "anomalous"] <-
+          as.character(flag_scheme[2])
+      }
     }
+    return(data)
   }
-
-  return(data)
-}
