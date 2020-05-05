9b8abe51a11908168551161bfbbf3a00e0364c87
Christina Bonsell <cbonsell@utexas.edu>
2020-05-05 10:56:18 -0500

    added parameter ref_cond to deal with conductivities in different units

--- R/flag_salinity.R
+++ R/flag_salinity.R
@@ -7,6 +7,7 @@
 #' @param condcol (character) Name of column for conductivity. Defaults to "conductivity".
 #' @param Terror (numeric) Precision for temperature.
 #' @param Cerror (numeric) Precision for conductivity.
+#' @param ref_cond (numeric) Reference conductivity value for calculating salinity, defaults to 42.9 for conductivity measured in miliSeimens/m^2/s.
 #' @param flag_colname (character) Column name to give the new flag column, defaults to "anomalous".
 #' @param flag_scheme (character) Scheme for flagging anomalous salinity values. Supply a vector of character flags to denote good/bad data respectively, e.g. c("valid", "invalid"). The first two will be used. IF NULL function will output a logical TRUE/FALSE column.
 #' @return (data.frame) The input dataframe with a new column added, named by the flag_colname argument, defaults to "anomalous" (character or logical) indicating anomalous values.
@@ -18,6 +19,7 @@ flag_salinity <-
            condcol = "conductivity",
            Terror,
            Cerror,
+           ref_cond,
            flag_colname = "anomalous",
            flag_scheme = c("valid", "invalid")) {
     stopifnot(is.data.frame(data), is.numeric(Terror), is.numeric(Cerror), is.null(flag_scheme) | length(flag_scheme) >= 2)
@@ -27,7 +29,7 @@ flag_salinity <-
 
     posTerror <- data[[tempcol]] + Terror
     posCerror <- data[[condcol]] + Cerror
-    pCpTSalerror <- calculate_salinity(posCerror, posTerror)
+    pCpTSalerror <- calculate_salinity(posCerror, posTerror, ref_cond)
 
     # data is "anomalous" if data +C+T error is below the freezing line
     data[[flag_colname]] <-
